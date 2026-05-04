c0622f9d51981876a66e31df09e489edf8c9a1b8
DarrylWong <darryl@cockroachlabs.com>
2026-05-04 16:18:22 -0400

    logical: transactional LDR rejects computed columns in constraints
    This change rejects any schemas that have either virtual or
    stored columns in constraints. Support for stored columns is
    tracked in #164507. Virtual columns would require the lock
    synthesis to support expression evaluation, which we do not
    plan to support for now.
    
    Fixes: https://github.com/cockroachdb/cockroach/issues/164506

--- pkg/crosscluster/logical/create_logical_replication_stmt.go
+++ pkg/crosscluster/logical/create_logical_replication_stmt.go
@@ -491,7 +491,13 @@ func doLDRPlan(
 				}
 			}
 
-			err := tabledesc.CheckLogicalReplicationCompatibility(&srcExternalCatalog.Tables[i], destTableDesc.TableDesc(), details.SkipSchemaCheck || details.CreateTable, writer == sqlclustersettings.LDRWriterTypeLegacyKV)
+			err := tabledesc.CheckLogicalReplicationCompatibility(
+				&srcExternalCatalog.Tables[i],
+				destTableDesc.TableDesc(),
+				details.SkipSchemaCheck || details.CreateTable,
+				writer == sqlclustersettings.LDRWriterTypeLegacyKV,
+				details.Mode == jobspb.LogicalReplicationDetails_Transactional,
+			)
 			if err != nil {
 				return err
 			}
