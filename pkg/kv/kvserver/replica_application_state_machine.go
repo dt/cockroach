bac7df48a7e9ab5ed831a7fc57a7f3dfe32473e7
Pavel Kalinnikov <pavel@cockroachlabs.com>
2026-05-07 10:47:30 +0100

    kvserver: rm unnecessary Replica.mu locking
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_application_state_machine.go
+++ pkg/kv/kvserver/replica_application_state_machine.go
@@ -151,13 +151,12 @@ func (sm *replicaStateMachine) NewBatch() apply.Batch {
 	// NewBatch (if it needs to read its own writes, which is unlikely).
 	b.batch = r.store.batchFactory.NewBatch()
 
-	r.mu.RLock()
+	r.raftMu.AssertHeld()
 	b.state = r.shMu.state
 	b.truncState = r.asLogStorage().shMu.trunc
 	b.state.Stats = &sm.stats
 	*b.state.Stats = *r.shMu.state.Stats
-	b.closedTimestampSetter = r.mu.closedTimestampSetter
-	r.mu.RUnlock()
+	b.closedTimestampSetter = r.raftMu.closedTimestampSetter
 	b.start = timeutil.Now()
 	return b
 }
